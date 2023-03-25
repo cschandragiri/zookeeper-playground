f6a3a3659506711f87c7789af6ebbef93e24de3e
Chinmay Chandragiri <cschandragiri@gmail.com>
2023-03-25 07:28:36 -0700

    Use tagged images

--- leader-election/src/main/java/demo/election/LeaderSelectorListener.java
+++ leader-election/src/main/java/demo/election/LeaderSelectorListener.java
@@ -17,8 +17,8 @@ public class LeaderSelectorListener extends LeaderSelectorListenerAdapter {
         log.info(getenv("INSTANCE_ID") + " is the leader now.");
         final Random random = new Random();
         log.info("Doing some random work");
-        //Sleep for a random time between 1min-5mins
-        Thread.sleep(random.nextInt(240000) + 60000);
+        //Sleep for a random time between 10-1min
+        Thread.sleep(random.nextInt(50000) + 10000);
         log.info(getenv("INSTANCE_ID") + " is giving up leadership.");
     }
 }
